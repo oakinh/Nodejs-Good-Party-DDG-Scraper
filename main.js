42efd1b4fa48bb6eb17ff035625b5fd96189382a
oakinh <nikaohill@gmail.com>
2024-05-30 17:02:44 -0600

    Update main for debugging

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
-import { searchDDG, extractTextFromHTML, analyzeResults } from './site-search.js';
+import { searchDDG, extractTextFromHTML, analyzeResults, analyzeResultsHasData } from './site-search.js';
 import { parseCSV, usStateSwitch, filterSearchResults } from './data-prep.js';
 import fs from 'fs';
+import { sleep } from './sleep.js';
 
 dotenv.config();
 
-const locations = parseCSV('./county.csv')?.filter(r=> /\bSC/i.test(r.state))?.slice(0, 3);
+const locations = parseCSV('./county.csv')?.filter(r=> /\bSC/i.test(r.state))?.slice(0, 1);
 console.log(locations.length);
-const limit = 10;
+const limit = 5;
 
 
 async function main() {
@@ -16,18 +17,18 @@ async function main() {
             const stateFullName = usStateSwitch(location.state);
             const locationClean = `${location.name} county ${location.state}`;
             const query = `${locationClean} local (election | candidate | elections | candidates) (filing | listing | filings | listings | list) 2024`;
-            const searchResults = await searchDDG(query, limit);
+            const searchResultsURL = await searchDDG(query, limit);
             // const queryObj = {state:location.state,stateFullName:stateFullName,county:location.name}
             // const filteredResults = filterSearchResults(searchResults, queryObj);
             const HTMLText = await extractTextFromHTML(searchResultsURL);
-            const candidateInformation = await analyzeResults(HTMLText, locationClean);
-            console.log(candidateInformation);
+            // const candidateInformation = await analyzeResultsHasData(HTMLText);
+            console.log(`Here's the HTML Text: ${HTMLText}`)
+            // console.log(candidateInformation);
         } catch (error) {
             console.error('An error occured:', error);
         }
-    }
-    
-    
+        await sleep(5000);
+    }  
 }
 
 main();
